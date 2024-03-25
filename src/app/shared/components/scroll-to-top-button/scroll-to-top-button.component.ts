871e869494119303294ba2c6972b2c2c911451df
nelrashidy <89921231+nelrashidy@users.noreply.github.com>
2024-03-25 21:48:49 +0200

    projecrStucture

---
+++ src/app/shared/components/scroll-to-top-button/scroll-to-top-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-scroll-to-top-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './scroll-to-top-button.component.html',
+  styleUrl: './scroll-to-top-button.component.scss'
+})
+export class ScrollToTopButtonComponent {
+
+}
