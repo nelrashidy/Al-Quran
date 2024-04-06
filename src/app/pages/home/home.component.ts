f31161e35f52deadd08f3d5d3e81ac2e53a1f081
nelrashidy <89921231+nelrashidy@users.noreply.github.com>
2024-04-07 00:35:04 +0200

    ..

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HomeHeaderComponent } from './components/home-header/home-header.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HomeHeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
