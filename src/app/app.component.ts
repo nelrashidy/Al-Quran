37c29df086a3018e749acf1baa6de233a1a855b0
nelrashidy <89921231+nelrashidy@users.noreply.github.com>
2024-04-06 21:44:16 +0200

    ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { ScrollToTopButtonComponent } from './shared/components/scroll-to-top-button/scroll-to-top-button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent , ScrollToTopButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
