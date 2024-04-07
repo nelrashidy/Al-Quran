13bcfd05da0523e554edcd09ec40326bb39bf31d
nelrashidy <89921231+nelrashidy@users.noreply.github.com>
2024-04-07 21:52:26 +0200

    ..

--- src/app/pages/home/components/home-services/home-services.component.ts
+++ src/app/pages/home/components/home-services/home-services.component.ts
@@ -9,5 +9,12 @@ import { CardComponent } from '../../../../shared/components/card/card.component
   styleUrl: './home-services.component.scss'
 })
 export class HomeServicesComponent {
+services:any[]=[
+  {id:0,imageUrl:'assets/images/qurann.png',title:'المصحف'},
+  {id:1,imageUrl:'assets/images/reciter.png',title:'تلاوة'},
+  {id:2,imageUrl:'assets/images/praying.png',title:'مواقيت الصلاة'},
+  {id:3,imageUrl:'assets/images/tasbih.png',title:'الأذكار'},
+  // {id:4,imageUrl:'assets/images/tasbih.png',title:'الدعاء'},
 
+]
 }
--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -8,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
+
+  @Input() imageUrl: string = '';
+  @Input() title: string='';
 
 }
